b1ca018f431c948bfc3f780ec8d2cb454bfeeefb
Moe @ moe1 <moe@moe1>
2019-03-01 12:53:54 -0600

    streamlined lookat code

--- src/hlpr_lookat/look_at_service.py
+++ src/hlpr_lookat/look_at_service.py
@@ -1,200 +1,135 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2016, HLP-R
+
+# Copyright (c) 2017, Elaine Short, SIM Lab
 # All rights reserved.
-#
+# 
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#  notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#  copyright notice, this list of conditions and the following
-#  disclaimer in the documentation and/or other materials provided
-#  with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#  contributors may be used to endorse or promote products derived
-#  from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Authors: Baris Akgun 
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# 
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# 
+# * Neither the name of the SIM Lab nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import math
 
 import rospy
-import tf2_ros
-import time
-import PyKDL
-
-from geometry_msgs.msg import Vector3, Transform, PoseStamped, Point
-import tf2_geometry_msgs
-from hlpr_lookat.srv import LookAt, LookAtResponse, LookAtT, LookAtTResponse, LookAtTS, LookAtTSResponse
-
-from hlpr_lookat.look_at_kinematics import LookAtKin
+import tf
 from hlpr_lookat.pan_tilt import PanTilt
+from hlpr_lookat.srv import LookAt,  LookAtRequest, LookAtResponse, LookAtT, LookAtTResponse, LookAtTS, LookAtTSRequest, LookAtTSResponse
+from hlpr_lookat.pan_tilt import PanTilt
+from geometry_msgs.msg import PoseStamped, Point
+import geometry_msgs
 
-def transform_to_kdl(t):
-  return PyKDL.Frame(PyKDL.Rotation.Quaternion(t.rotation.x, t.rotation.y,
-                                               t.rotation.z, t.rotation.w),
-                     PyKDL.Vector(t.translation.x,
-                                  t.translation.y,
-                                  t.translation.z))
+class LookatServer:
+    def __init__(self):
+        self.base_name = "hlpr_lookat"
+        rospy.init_node(self.base_name + '_server')
+        self.pt = PanTilt(queue_size=1) #this will handle the joint limits
 
-def transform_vec3(in_vec3, transform_frame):
-  p = transform_to_kdl(transform_frame) * PyKDL.Vector(in_vec3.x, in_vec3.y, in_vec3.z)
-  out_vec3 = Vector3()
-  out_vec3.x = p[0]
-  out_vec3.y = p[1]
-  out_vec3.z = p[2]
-  return out_vec3
+        self.default_frame = "base_link"
+        self.head_frame = "head_link"
+        
+        self.listener = tf.TransformListener()
+        rospy.sleep(1.0)
+        
+        #s_v3 = rospy.Service(self.base_name + '_vec3', LookAt, self.handle_req_v3)
+        #s_tr  = rospy.Service(self.base_name + '_tr', LookAtT, self.handle_req_tr)
+        #s_s_tt = rospy.Service(self.base_name + '_s_tr', LookAtTS, self.handle_req_s_tr)
 
-class LookAtService:
-  def __init__(self, name = 'lookat', init_service = False, base_to_pan_transform = None):
-    self.pt = PanTilt()
-    self.head = LookAtKin()
-    self.base_name = name
+    
+    def handle_req_v3(self, req):
+        resp = LookAtResponse()
+        resp.success = self.lookat(req.desired_position)
+        return resp
 
-    self.tfBuffer = None 
-    self.listener = None 
+    def handle_req_tr(self, req):
+        resp = LookAtTResponse()
+        resp.success = self.lookat(req.desired_tr.translation)
+        return resp
 
-    self.base = 'base_link'
-    self.lookat_root = 'pan_base_link'
+    def handle_req_s_tr(self, req):
+        resp = LookAtTSResponse()
+        
+        resp.success = self.lookat(req.desired_s_tr.transform.translation,
+                                   req.desired_s_tr.transform.rotation,
+                                   req.desired_s_tr.child_frame_id)
+        return resp
 
-    if base_to_pan_transform is None:
-      self._def_base2pantilt()
-    else:
-      self.base2pantilt = base_to_pan_transform
+    # need a head link w/ z up & x out
+    def pos2theta(self, pos):
+        tilt = math.atan2(pos.z,pos.x)
+        pan = math.atan2(pos.y,pos.x)
+        return (pan, tilt)
 
+    def lookat(self, pose, orientation = None, pos_frame = None):
+        if orientation is None:
+            orientation = geometry_msgs.msg.Quaternion()
+            orientation.x = 0.0
+            orientation.y = 0.0
+            orientation.z = 0.0
+            orientation.w = 1.0
 
-    if init_service:
-      self.init_service()
+        if pos_frame is None:
+            pos_frame = self.default_frame
 
-  #to be used in converting from base_link to pantilt base
-  def _def_base2pantilt(self):
+        if pos_frame==self.head_frame:
+            position = pose
+        else:
+            try:
+                rospy.loginfo(
+                    "looking up transform from frame {} to frame {}".format(
+                        pos_frame, self.head_frame))
+                # Get position from the request
+                pos = PoseStamped()
+                pos.pose.position = pose
+                pos.header.frame_id = pos_frame
+                pos.pose.orientation = orientation
 
-    self.base2pantilt = Transform()
-    self.base2pantilt.translation.x = -0.395
-    self.base2pantilt.translation.y =  0.0
-    self.base2pantilt.translation.z = -1.505
+            
+                # Lookup the transform between frames
+                trans = self.listener.transformPose(self.head_frame, pos)
 
-    self.base2pantilt.rotation.x = 0.0
-    self.base2pantilt.rotation.y = 0.0
-    self.base2pantilt.rotation.z = 0.0
-    self.base2pantilt.rotation.w = 1.0
-  
-  def _def_base2pantilt_custom(self, x,y,z):
+                position = trans.pose.position
 
-      #self.base2pantilt.translation.x = -0.202
-      #self.base2pantilt.translation.y =  0.0
-      #self.base2pantilt.translation.z = -1.440
-      self.base2pantilt.translation.x = x
-      self.base2pantilt.translation.y = y
-      self.base2pantilt.translation.z = z
-  
-  def lookat(self,in_vec3):
-    des_pos = transform_vec3(in_vec3, self.base2pantilt)
-    theta = self.head.headIK([des_pos.x, des_pos.y, des_pos.z])
-    #print theta
-    if theta[0] is None:
-      return False
-    else:
-      self.pt.set_pan_tilt(theta)
-      return True
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                rospy.logwarn("Cannot look up translation, not moving head.")
+                return False
+        
+        pan, tilt = self.pos2theta(position)
+        print pan, tilt
+        self.pt.set_pan_tilt(pan, tilt)
+            
+if __name__=="__main__":
+    srv = LookatServer()
 
-  def handle_req_v3(self,req):
-    resp = LookAtResponse()
-    resp.success = self.lookat(req.desired_position)
-    return resp
+    req = LookAtTSRequest()
+    req.desired_s_tr.transform.translation.x = 1.0
+    req.desired_s_tr.transform.translation.y = 0.0
+    req.desired_s_tr.transform.translation.z = 0.0
+    
+    req.desired_s_tr.transform.rotation.x = 0.0
+    req.desired_s_tr.transform.rotation.y = 0.0
+    req.desired_s_tr.transform.rotation.z = 0.0
+    req.desired_s_tr.transform.rotation.w = 1.0
+    
+    req.desired_s_tr.child_frame_id = "/head_link"
 
-  def handle_req_tr(self, req):
-    resp = LookAtTResponse()
-    resp.success = self.lookat(req.desired_tr.translation)
-    return resp
-
-  def handle_req_s_tr(self,req):
-    resp = LookAtTSResponse()
-    try:
-      # Get position from the request
-      pos = req.desired_s_tr.transform.translation
-
-      # Lookup the transform between frames
-      trans = self.tfBuffer.lookup_transform(self.base, req.desired_s_tr.child_frame_id, rospy.Time(0))
-
-      # Apply the transform to the requested position
-      pose = PoseStamped()
-      pose.pose.position = Point(pos.x, pos.y, pos.z)
-      pose.pose.orientation = req.desired_s_tr.transform.rotation
-      trans = tf2_geometry_msgs.do_transform_pose(pose, trans).pose.position
-
-      # Send off the request
-      resp.success = self.lookat(trans)
-
-    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-      print 'Cannot lookupTransform'
-      resp.success = False
-
-    return resp
-
-  def init_service(self, spin = True):
-    rospy.init_node(self.base_name + '_server')
-
-    # Defaults to 10 if we don't have this parameter set
-    self.pt = PanTilt(queue_size=1)
-
-    # Check what robot we're using - default false
-    self.poli_urdf = rospy.get_param("~poli_urdf", False)
-    self.verbose = rospy.get_param("~verbose", False)
-    if self.poli_urdf:
-      rospy.loginfo("Poli URDF Flag set to  %s" % self.poli_urdf)
-
-      # Generate a different IK solution using new poli urdf offsets
-      pan_limits = [-0.75, 0.75]
-      tilt_limits = [-0.25, 0.589]
-      self._def_base2pantilt_custom(-0.202, 0.0, -1.440)
-      self.head = LookAtKin(params=[-0.131, 0.0425, (-0.04201-0.03425) ,(0.0245+0.1125)],
-                            pan_limits=pan_limits,
-                            tilt_limits=tilt_limits, verbose=self.verbose)
-      self.pt = PanTilt(pan_limits=pan_limits, tilt_limits=tilt_limits, queue_size=1)
-
-    else:
-      self.head = LookAtKin(verbose=self.verbose)
-
-    if self.verbose:
-      rospy.loginfo("hlpr_lookat service is in Verbose mode")
-
-    self.tfBuffer = tf2_ros.Buffer()
-    self.listener = tf2_ros.TransformListener(self.tfBuffer)
-    #waiting 1 second to have these guys initialized
-    time.sleep(1)
-
-    if not isinstance(self.base2pantilt, Transform):
-      print 'What are you doing?'
-      try:
-        trans = self.tfBuffer.lookup_transform(self.lookat_root, self.base, rospy.Time(0))
-        self.base2pantilt = trans.transform
-      except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-        print 'Cannot lookupTransform, going with default'
-        self._def_base2pantilt()
-
-    s_v3 = rospy.Service(self.base_name + '_vec3', LookAt, self.handle_req_v3)
-    s_tr  = rospy.Service(self.base_name + '_tr',   LookAtT, self.handle_req_tr)
-    s_s_tt = rospy.Service(self.base_name + '_s_tr', LookAtTS, self.handle_req_s_tr)
-
-    if spin:
-      rospy.spin()
-
-if __name__ == "__main__":
-  loook_at = LookAtService(init_service = True)
-  
+    srv.handle_req_s_tr(req)
--- src/hlpr_lookat/pan_tilt.py
+++ src/hlpr_lookat/pan_tilt.py
@@ -38,6 +38,7 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 import rospy
 import time
+import os
 from math import *
 
 def clamp(x, limits):
@@ -47,7 +48,7 @@ class PanTilt:
   #limits in form min, max, zero point
   def __init__(self, pan_limits=None, tilt_limits=None, queue_size=10):
     self.robot = rospy.get_param("/ROBOT_NAME", "default")
-
+    
     if pan_limits is None and self.robot=="poli2":
       self.pan_limits  = [-3.14,3.14, 0]
     elif pan_limits is None:
@@ -56,7 +57,7 @@ class PanTilt:
       self.pan_limits = pan_limits
 
     if tilt_limits is None and self.robot=="poli2":
-      self.tilt_limits  = [0.0,1.2,0.75]
+      self.tilt_limits  = [0.0,0.9,0.65] #[0.0,1.2,0.65]<-use once wires go through neck
       self.tilt_conv = lambda x: -x
     elif tilt_limits is None:
       self.tilt_limits = [-pi/3., pi/3., 0]
@@ -64,35 +65,70 @@ class PanTilt:
     else:
       self.tilt_limits = tilt_limits
 
-    self.pub_tilt = rospy.Publisher('/tilt_controller/command', Float64, queue_size=queue_size)
-    self.pub_pan  = rospy.Publisher('/pan_controller/command', Float64, queue_size=queue_size)
-
     #pan/tilt position in unknown state to start
     self.tilt_pos = None
     self.pan_pos = None
-    
+
     rospy.Subscriber("/tilt_controller/state", JointState, self.cb_tilt)
     rospy.Subscriber("/pan_controller/state", JointState, self.cb_pan)
 
+    if self.robot=="poli2":
+      self.pub_tilt = rospy.Publisher('/tilt_motor/position_controller/command', Float64, queue_size=queue_size)
+      self.pub_pan  = rospy.Publisher('/pan_motor/position_controller/command', Float64, queue_size=queue_size)
+      rospy.Subscriber("/tilt_motor/joint_states", JointState, self.cb_tilt)
+      rospy.Subscriber("/pan_motor/joint_state", JointState, self.cb_pan)
+    else:
+      self.pub_tilt = rospy.Publisher('/tilt_controller/command', Float64, queue_size=queue_size)
+      self.pub_pan  = rospy.Publisher('/pan_controller/command', Float64, queue_size=queue_size)
+    rospy.sleep(0.5)
+
+
+    while self.pan_pos is None or self.tilt_pos is None:
+      rospy.sleep(0.1)
+      rospy.logwarn_throttle(1,"Waiting for pan/tilt position publishers...")
     
-  def set_pan(self, pan_position):
+    
+  def set_pan(self, pan_position, wait=False):
     pan_position += self.pan_limits[2]
     
     pos = clamp(pos, self.pan_limits)
     self.pub_pan.publish(pan_position)
+    if wait:
+      self.wait()
 
-  def set_tilt(self, tilt_position):
-    tilt_position += self.tilt_limits[2]
-    
-    pos = clamp(pos, self.tilt_limits)
+  def set_tilt(self, tilt_position, wait=False):
+    pos = clamp(tilt_position + self.tilt_limits[2], self.tilt_limits)
+    if self.robot=="poli2":
+      tilt = -1*tilt
     self.pub_tilt.publish(self.tilt_conv(tilt_position))
+    if wait:
+      self.wait()
 
-  def set_pan_tilt(self, pan_position, tilt_position):
-    pan = clamp(pan_position, self.pan_limits)
-    tilt = clamp(tilt_position, self.tilt_limits)
+  def set_pan_tilt(self, pan_position, tilt_position, wait=False):
+    print "requested pan/tilt:", pan_position, tilt_position 
+    pan = clamp(pan_position + self.pan_limits[2], self.pan_limits)
+    tilt = clamp(tilt_position + self.tilt_limits[2], self.tilt_limits)
+    if self.robot=="poli2":
+      tilt = -1*tilt
+    print "setting pan/tilt:", pan, tilt
     self.pub_pan.publish(pan)
     self.pub_tilt.publish(tilt)
-    rospy.sleep(3)
+    if wait:
+      self.wait()
+
+  #could be up to 0.5s delayed from actual end of movement
+  def wait(self):
+    changed = True
+    self.prev_pan = self.pan_pos
+    self.prev_tilt = self.tilt_pos
+    while(changed):
+      rospy.sleep(0.5)
+      d_pan = abs(self.pan_pos-self.prev_pan)
+      d_tilt = abs(self.tilt_pos-self.prev_tilt)
+      if d_pan<0.01 and d_tilt<0.01:
+        changed = False
+      self.prev_pan = self.pan_pos
+      self.prev_tilt = self.tilt_pos
 
   def cb_tilt(self, js):
     self.tilt_pos = js.position[0]
@@ -103,9 +139,10 @@ class PanTilt:
 if __name__ == "__main__":
   rospy.init_node('pan_tilt_test', anonymous=True)
   pt = PanTilt()
-  search_pattern = [[0.0,0.7], [0.3, 0.6],[-0.3, 0.6],[-0.3, 0.8],[0.3, 0.8]]
+  search_pattern = [[0.0,-0.8], [0.3, 0.3],[-0.3, 0.2],[-0.3, -0.8],[0.3, 0.1]]
   for i in range(0, len(search_pattern)):
-    pt.set_pan_tilt(search_pattern[i])
-    time.sleep(0.5)
-  pt.set_pan_tilt([0,0])
+    pt.set_pan_tilt(search_pattern[i][0], search_pattern[i][1], wait=True)
+    print("Done")
+    rospy.sleep(2.0)
+  pt.set_pan_tilt(0,0, wait=True)
 
