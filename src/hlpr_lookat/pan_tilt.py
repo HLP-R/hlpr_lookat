2097ef97a7f48054540db2bcaba1f5b3fc763370
Poli1 <poli@poli1>
2019-05-03 16:13:17 -0500

    changed pan limits for poli2 robots

--- src/hlpr_lookat/pan_tilt.py
+++ src/hlpr_lookat/pan_tilt.py
@@ -53,7 +53,7 @@ class PanTilt:
     rospy.loginfo("Launching pan/tilt with robot name " + self.robot)
 
     if pan_limits is None and self.robot=="poli2":
-      self.pan_limits  = [-1.57,1.57, 0]
+      self.pan_limits  = [-pi/3,pi/3, 0]
     elif pan_limits is None:
       self.pan_limits  = [-pi/3.,pi/2., 0]
     else:
